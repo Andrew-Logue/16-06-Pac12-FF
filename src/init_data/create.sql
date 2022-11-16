03b9682e774167d4a038a29d828369aca9ff55c2
Ethan Trapp <ettr6128@colorado.edu>
2022-11-16 12:53:05 -0700

    /updatePlayerTable now clears table before updating

--- src/init_data/create.sql
+++ src/init_data/create.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS users;
 CREATE TABLE users(
     username VARCHAR(50) PRIMARY KEY,
     password CHAR(60) NOT NULL
@@ -17,8 +18,10 @@ CREATE TABLE users_teams(
     team_id INTEGER NOT NULL REFERENCES teams (team_id)
 );
 
+DROP TABLE IF EXISTS players;
 create table players(
-    name varchar(50) primary key,
+    player_id serial primary key,
+    name varchar(50) not null,
     team varchar(50) not null,
     number smallint,
     position varchar(10)
